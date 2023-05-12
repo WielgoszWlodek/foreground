e101fbc76509e2677d026df7df7a0bc770a17275
WielgoszWlodek <132360939+WielgoszWlodek@users.noreply.github.com>
2023-05-12 19:39:24 +0200

    Update Register.jsx

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -14,13 +14,13 @@ export default function Register() {
     e.preventDefault();
     setError(false);
     try {
-      const res = await axios.post("https://emotioapi.onrender.com/auth/register", {
+      const res = await axios.post("/auth/register", {
         username,
         email,
         password,
       });
       console.log(res) 
-      res.data && window.location.replace("https://emotioapi.onrender.com/login");
+      res.data && window.location.replace("/login");
     } catch (err) {
       setError(true);
     }
